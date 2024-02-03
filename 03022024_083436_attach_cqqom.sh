7f4a093aa72f594299e504fdc754218a4c61a1b4
xerxemali <kvekveskirinika682@gmail.com>
2024-02-03 08:34:36 +0000

    Changing attach

---
+++ 03022024_083436_attach_cqqom.sh
@@ -0,0 +1,1 @@
+Singer Alice Cooper once had a live chicken thrown at him during a concert in Toronto. He threw the chicken back at the crowd and all the publicity surrounding the incident skyrocketed his singing career
